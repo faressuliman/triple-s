c95b5285ebb1ea8c66b7c97d2d5f7d4fa6157704
Frontman <faresashraf20044@gmail.com>
2025-08-01 14:33:21 +0200

    Initial commit: project setup with routing and basic pages

---
+++ src/router/index.tsx
@@ -0,0 +1,22 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from "react-router-dom";
+
+import Home from "../pages";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Layout from "../pages/Layout";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+        </Route>
+    )
+);
+
+export default router
