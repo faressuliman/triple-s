f41fb14ddb5b57ec0ce5f67b644056a5db231fef
Frontman <faresashraf20044@gmail.com>
2025-08-14 23:22:55 +0200

    forwardRef for the TextArea component

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -1,15 +1,18 @@
-import type { TextareaHTMLAttributes } from "react";
+import { forwardRef, type Ref, type TextareaHTMLAttributes } from "react";
 
 interface IProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = ({ ...rest }: IProps) => {
+const Textarea = forwardRef(({ ...rest }: IProps, ref: Ref<HTMLTextAreaElement>) => {
   return (
     <textarea
+    ref={ref}
       className="border-[1px] border-gray-300 shadow-md focus:border-[#22518f] focus:outline-none focus:ring-1 focus:ring-[#22518f] mb-1 rounded-lg px-3 py-3 text-md w-full bg-transparent"
       rows={6}
       {...rest}
     />
   );
-};
+});
+
+Textarea.displayName = "Textarea";
 
 export default Textarea;
