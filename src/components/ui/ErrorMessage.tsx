723db5c63f4c58d140750ff6764caa4643f191ac
faressuliman <faresashraf20044@gmail.com>
2025-11-19 21:55:50 +0200

    refactored errormessage component

--- src/components/ui/ErrorMessage.tsx
+++ src/components/ui/ErrorMessage.tsx
@@ -1,9 +1,18 @@
+import { Info } from "lucide-react";
+
 interface IProps {
   msg?: string;
 }
 
 const ErrorMessage = ({ msg }: IProps) => {
-  return msg ? <span className="block text-red-700 font-semibold text-sm">{msg}</span> : null;
+  return msg ? (
+    <>
+      <div className="flex justify-items-center items-center space-x-1">
+        <Info className="text-red-700 w-3 h-3"/>
+        <span className="block text-red-700 font-semibold text-xs tracking-wide"> {msg}</span>
+      </div>
+    </>
+  ) : null;
 };
 
 export default ErrorMessage;
